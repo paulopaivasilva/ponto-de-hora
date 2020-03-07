daa444ba3e5df9708b9ca85f3628320ae9411f52
Paulo Henrique <paulohenriquepaiva@ymail.com>
2020-03-07 12:09:34 -0500

    Adição da função de bater o ponto e de exibir dia atual

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage  implements OnInit {
 
   constructor() {}
+
+  private date: Date = new Date()
+  day: string = this.date.getDay().toString()
+  month: String = this.date.getMonth().toString()
+  year: String = this.date.getFullYear().toString()
+  hour: String
+  minutes: String
+  daily = { date: '', horarios: []}
+  
+  ngOnInit(){
+    this.daily.date = `${this.day}/${this.month}/${this.year}`
+    this.getTime()
+  }
+
+  setTime(){
+    this.hour = this.date.getHours().toString()
+    this.minutes = this.date.getMinutes().toString()
+    this.daily.horarios.push(this.hour + ":" + this.minutes)
+    localStorage.setItem('daily', JSON.stringify(this.daily))
+  }
+
+  getTime(){
+    
+  }
 
 }
